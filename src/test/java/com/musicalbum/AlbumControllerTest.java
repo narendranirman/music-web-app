b1d3a200787ee9f0c863f1d79c095a4b5813961a
narendrad <narendrad@menlo-technologies.com>
2018-10-25 20:25:51 +0530

    --Fixed test cases

--- src/test/java/com/musicalbum/AlbumControllerTest.java
+++ src/test/java/com/musicalbum/AlbumControllerTest.java
@@ -61,7 +61,7 @@ public class AlbumControllerTest extends AbstractMockServerTest{
 		String response = mvcResult.getResponse().getContentAsString();
 		List<Album> albumList = mapper.readValue(response, mapper.getTypeFactory().constructCollectionType(List.class, Album.class));
 		Assert.assertTrue(albumList.size() > 0);
-		Assert.assertTrue(mvcResult.getResponse().getStatus() == 400);
+		Assert.assertTrue(mvcResult.getResponse().getStatus() == 200);
 		
 	}
 	
